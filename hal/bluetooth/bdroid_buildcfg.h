99e0b0dd6484846ac13251134e10d8f7953c5a02
Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>
2019-05-30 16:29:42 +0300

    hal/bluetooth: Added custom configuration file bdroid_buildcfg.h
    Jira-Id: ADM-3193
    
    Signed-off-by: Oleksii Gulchenko <oleksii.gulchenko@globallogic.com>
    Change-Id: I7036e8c3664edd45decabdb0e6a49360de08d3b3

---
+++ hal/bluetooth/bdroid_buildcfg.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016 GlobalLogic
+ */
+
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BNEP_INCLUDED           FALSE
+#define BTA_PAN_INCLUDED        FALSE
+
+#define BTA_AV_INCLUDED         TRUE
+#define BTA_AV_SINK_INCLUDED    TRUE
+
+#define BTA_HS_INCLUDED         TRUE
+
+#define BTM_SCO_HCI_DEBUG       TRUE
+#define BTM_SCO_INCLUDED        TRUE
+
+#define BTM_SCO_HCI_INCLUDED    FALSE
+
+/* Handsfree device */
+#define BTA_DM_COD {0x20, 0x04, 0x08}
+
+#endif
