986d40f1be46261661aecf5ad098b0fefc42c859
Oleh Cherpak <oleh.cherpak@globallogic.com>
2020-07-16 10:58:04 +0300

    BoardConfig.mk: Use correct BOARD_WLAN_DEVICE
    Also remove unused include
    
    Jira-Id: ADM-4935
    
    Signed-off-by: Oleh Cherpak <oleh.cherpak@globallogic.com>
    Change-Id: I8aad32c4ea068c388702755bbc9acf14590c17e7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,8 +26,7 @@ ifeq ($(BOARD_WIFI_VENDOR), realtek)
     BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
     BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_rtl
     BOARD_HOSTAPD_DRIVER := NL80211
-    BOARD_WLAN_DEVICE := rtl
-    CFI_INCLUDE_PATHS += device/linaro/poplar/wifi/wpa_supplicant_8_lib
+    BOARD_WLAN_DEVICE := realtek
 endif
 
 
